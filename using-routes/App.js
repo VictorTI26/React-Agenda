06f26594c6afa8670b8acf98ac4a7baf323ab8c5
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2023-10-31 21:14:53 -0300

    Update App.js

--- using-routes/App.js
+++ using-routes/App.js
@@ -13,7 +13,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Adicionar Lista" component={ListScreen} />
+        <Stack.Screen name="ADD list" component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
