59e705229fec9bbf34c9e66531cdb8be6e9c0fd0
VictorTI26 <128429841+VictorTI26@users.noreply.github.com>
2023-11-12 13:39:45 -0300

    Update HomeScreen.js

--- using-routes/src/HomeScreen/HomeScreen.js
+++ using-routes/src/HomeScreen/HomeScreen.js
@@ -1,60 +1,68 @@
-// HomeScreen.js
-
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, View, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 function HomeScreen() {
   const navigation = useNavigation();
-  const [quantidadeLista, setQuantidadeLista] = useState(0);
-  const [botoesAdicionais, setBotoesAdicionais] = useState([]);
+  const [listas, setListas] = useState([]);
+  const [nomeLista, setNomeLista] = useState('');
+  const [editandoIndex, setEditandoIndex] = useState(null);
 
-  const addList = () => {
-    if (quantidadeLista < 10) {
-      setQuantidadeLista(quantidadeLista + 1);
+  const addOrEditList = () => {
+    if (nomeLista.trim() === '') {
+      alert('Digite um nome para a lista');
+      return;
+    }
 
-      if (quantidadeLista < 10) {
-        const novoBotao = (
-          <Button
-            key={quantidadeLista}
-            title={`Botão ${quantidadeLista}`}
-            onPress={() => {
-              navigation.navigate('ListScreen'); // Navega para a página "ListScreen"
-            }}
-          />
-        );
-        setBotoesAdicionais([...botoesAdicionais, novoBotao]);
-      }
+    if (editandoIndex !== null) {
+      const listasAtualizadas = [...listas];
+      listasAtualizadas[editandoIndex].nome = nomeLista;
+      setListas(listasAtualizadas);
+      setEditandoIndex(null);
     } else {
-      alert('Você atingiu o limite de 10 listas');
-    }
-  }
-
-  const deleteList = () => {
-    if (quantidadeLista > 0) {
-      setQuantidadeLista(quantidadeLista - 1);
-
-      if (botoesAdicionais.length > 0) {
-        setBotoesAdicionais(botoesAdicionais.slice(0, -1));
+      if (listas.length < 10) {
+        setListas([...listas, { id: listas.length, nome: nomeLista }]);
+      } else {
+        alert('Você atingiu o limite de 10 listas');
       }
     }
-  }
+
+    setNomeLista('');
+  };
+
+  const editList = (index) => {
+    setNomeLista(listas[index].nome);
+    setEditandoIndex(index);
+  };
+
+  const deleteList = (index) => {
+    setListas(listas.filter((_, i) => i !== index));
+  };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Suas listas</Text>
+      <TextInput
+        placeholder="Nome da lista"
+        value={nomeLista}
+        onChangeText={setNomeLista}
+        style={{ marginBottom: 10, borderWidth: 1, borderColor: 'gray', padding: 5 }}
+      />
 
-      {quantidadeLista < 10 && (
-        <Button title="add list" onPress={addList} />
-      )}
+      <Button title={editandoIndex !== null ? "Salvar alteração" : "Adicionar Lista"} onPress={addOrEditList} />
 
-      {botoesAdicionais.length > 0 && (
-        <Button title="delete list" onPress={deleteList} />
-      )}
-
-      <Text>Você tem {quantidadeLista}/10</Text>
-
-      {botoesAdicionais.map((botao) => botao)}
+      {listas.map((lista, index) => (
+        <View key={lista.id} style={{ marginTop: 10, width: '15%' }}>
+          <Button
+            title={lista.nome}
+            onPress={() => navigation.navigate('ListScreen', { nomeLista: lista.nome })}
+          />
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 5 }}>
+            <Button title="Editar" onPress={() => editList(index)} />
+            <Button title="Excluir" onPress={() => deleteList(index)} />
+          </View>
+        </View>
+      ))}
     </View>
   );
 }
